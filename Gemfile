3fd62add222889b5426030cbf9f60cf8d83e5f4d
Josh Orr <josh.orr@kajabi.com>
2020-09-17 11:48:24 -0500

    mkdir command not working, committing current code jic rollback on mkdir fixes

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+
+# gem "rails"
+gem 'rubysl-shellwords', '~> 2.0'
+gem 'pry', '~> 0.13.1'
--- notion_converter.rb
+++ notion_converter.rb
@@ -1,6 +1,9 @@
 #! /usr/bin/ruby
-def walk_tree(directory)
-    #puts "Directory arg #{directory}"
+require 'shellwords'
+require 'pry'
+
+def walk_tree(directory, target_dir)
+    puts "Directory arg #{directory}"
     Dir.chdir(directory)
     puts "working dir #{Dir.pwd}"
     arr = Dir.entries(Dir.pwd)
@@ -11,22 +14,40 @@ def walk_tree(directory)
             puts "convert this #{fs_obj}"
         elsif fs_obj.end_with?('.csv') || fs_obj.end_with?('.png')
             puts "copy this #{fs_obj}"
-        else
+        elsif File.directory?("#{directory}/#{fs_obj}")
             puts "this is a dir #{fs_obj}"
-            walk_tree("#{Dir.pwd}/#{fs_obj}")
+            target_path = "#{directory}/#{fs_obj}"
+            target_path = target_path.gsub(directory, target_dir)
+            system("mkdir -p \"#{target_path}\"")
+            begin
+                walk_tree("#{directory}/#{fs_obj}", target_dir)
+            rescue Exception => ex
+                puts ex
+            end
+        else
+            puts "Don't know what to do with #{fs_obj}"
         end
     end
 end
+
+def convert(file_path)
+#use pandoc command to convert markdown to docx
+end
+
+def copy_to_converted(file_path, converted_dir)
+end
+
+# Main
 if ARGV.length < 2
     puts "Not enough args, the format is `./notion_converter {notion_export_dir} {target_dir}`"
     exit
 end
 
-export_dir = ARGV[0]
+export_dir = "#{Dir.pwd}/#{ARGV[0]}"
 target_dir = ARGV[1]
 
 #arr = Dir.entries(export_dir)
-walk_tree(export_dir)
+walk_tree(export_dir, target_dir)
 
 
-Dir.mkdir(target_dir) unless Dir.exists?(target_dir)
+#Dir.mkdir(target_dir) unless Dir.exists?(target_dir)
